f6276ef1b32f148412718f5262ebf3cb265dccd0
safaaaz <58644583+safaaaz@users.noreply.github.com>
2020-01-05 16:31:01 +0200

    Add files via upload

---
+++ image.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import filedialog
+import sqlite3
+top=Tk()
+top.geometry('1000x600')
+
+
+image=filedialog.askopenfilename(filetypes=[("Image File",'.png')])
+print(image.split('/')[-1::][0])
+conn = sqlite3.connect("Accept.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS pic (name TEXT)")
+cursor.execute("INSERT INTO pic (name)VALUES (?)",(image.split('/')[-1::][0],))
+conn.commit()
+im=PhotoImage(file=image)
+Label(top,image=im).pack()
+top.mainloop()
